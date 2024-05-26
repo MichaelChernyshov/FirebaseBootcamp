512a230ff3ba0d7285d877b0a5ea1d7c9ac80e74
Mikhail Chernyshov <chernyshov.michaele@icloud.com>
2024-05-26 12:36:50 +0200

    Initial Commit

---
+++ FirebaseBootcamp/FirebaseBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseBootcampApp.swift
+//  FirebaseBootcamp
+//
+//  Created by Mikhail Chernyshov on 26/5/24.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
