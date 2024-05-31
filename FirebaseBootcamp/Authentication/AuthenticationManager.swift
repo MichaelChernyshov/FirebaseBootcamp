ce68aab342827ec192ce5c1df080b3a478fa02c1
Mikhail Chernyshov <chernyshov.michaele@icloud.com>
2024-05-31 11:38:31 +0200

    Added new package, removed old. Added google authentication. Added extensions and utilities for UIViewController

--- FirebaseBootcamp/Authentication/AuthenticationManager.swift
+++ FirebaseBootcamp/Authentication/AuthenticationManager.swift
@@ -23,7 +23,6 @@ struct AuthDataResultModel {
 
 final class AuthenticationManager {
     static let shared = AuthenticationManager()
-    
     private init() {}
     
     func getAuthenticatedUser () throws -> AuthDataResultModel {
@@ -34,23 +33,57 @@ final class AuthenticationManager {
         return AuthDataResultModel(user: user)
     }
     
-    @discardableResult
-    func createUser(email: String, password: String) async throws -> AuthDataResultModel {
-        let authDataResult = try await Auth.auth().createUser(withEmail: email, password: password)
-        return AuthDataResultModel(user: authDataResult.user)
-    }
-    
-    @discardableResult
-    func signInUser(email: String, password: String) async throws -> AuthDataResultModel {
-        let authDataResult = try await Auth.auth().signIn(withEmail: email, password: password)
-        return AuthDataResultModel(user: authDataResult.user)
-    }
-    
-    func resetPassword (email: String) async throws {
-        try await Auth.auth().sendPasswordReset(withEmail: email)
-    }
-    
     func signOut() throws {
         try Auth.auth().signOut()
+    }
+}
+
+// MARK: SIGN IN EMAIL
+extension AuthenticationManager {
+    @discardableResult
+       func createUser(email: String, password: String) async throws -> AuthDataResultModel {
+           let authDataResult = try await Auth.auth().createUser(withEmail: email, password: password)
+           return AuthDataResultModel(user: authDataResult.user)
+       }
+       
+       @discardableResult
+       func signInUser(email: String, password: String) async throws -> AuthDataResultModel {
+           let authDataResult = try await Auth.auth().signIn(withEmail: email, password: password)
+           return AuthDataResultModel(user: authDataResult.user)
+       }
+       
+       func resetPassword(email: String) async throws {
+           try await Auth.auth().sendPasswordReset(withEmail: email)
+       }
+       
+       func updatePassword(password: String) async throws {
+           guard let user = Auth.auth().currentUser else {
+               throw URLError(.badServerResponse)
+           }
+           
+           try await user.updatePassword(to: password)
+       }
+       
+       func updateEmail(email: String) async throws {
+           guard let user = Auth.auth().currentUser else {
+               throw URLError(.badServerResponse)
+           }
+           
+           try await user.updateEmail(to: email)
+       }
+}
+
+// MARK: SIGN IN SSO
+
+extension AuthenticationManager {
+    @discardableResult
+    func signInWithGoogle(tokens: GoogleSignInResultModel) async throws -> AuthDataResultModel {
+        let credential = GoogleAuthProvider.credential(withIDToken: tokens.idToken, accessToken: tokens.accessToken)
+        return try await signIn(credential: credential)
+    }
+    
+    func signIn(credential: AuthCredential) async throws -> AuthDataResultModel {
+        let authDataResult = try await Auth.auth().signIn(with: credential)
+        return AuthDataResultModel(user: authDataResult.user)
     }
 }
--- FirebaseBootcamp/Authentication/AuthenticationView.swift
+++ FirebaseBootcamp/Authentication/AuthenticationView.swift
@@ -6,8 +6,39 @@
 //
 
 import SwiftUI
+import GoogleSignIn
+import GoogleSignInSwift
+import FirebaseAuth
+
+struct GoogleSignInResultModel {
+    let idToken: String
+    let accessToken: String
+}
+
+@MainActor
+final class AuthenticationViewModel: ObservableObject {
+    
+    func signInGoodle() async throws  {
+        guard let topVC = Utilities.shared.topViewController() else {
+            throw URLError(.cannotFindHost)
+        }
+        
+        let gidSignInResult = try await GIDSignIn.sharedInstance.signIn(withPresenting: topVC)
+        
+        guard let idToken = gidSignInResult.user.idToken?.tokenString else {
+            throw URLError(.badServerResponse)
+        }
+        let accessToken: String = gidSignInResult.user.accessToken.tokenString
+        
+        let tokens = GoogleSignInResultModel(idToken: idToken, accessToken: accessToken)
+        try await AuthenticationManager.shared.signInWithGoogle(tokens: tokens)
+
+        
+    }
+}
 
 struct AuthenticationView: View {
+    @StateObject private var vm = AuthenticationViewModel()
     @Binding var showSignInView: Bool
     
     var body: some View {
@@ -22,6 +53,17 @@ struct AuthenticationView: View {
                     .frame(maxWidth: .infinity)
                     .background(.blue)
                     .cornerRadius(10)
+            }
+            
+            GoogleSignInButton(viewModel: GoogleSignInButtonViewModel(scheme: .dark, style: .wide, state: .normal)) {
+                Task {
+                    do {
+                        try await vm.signInGoodle()
+                        showSignInView = false
+                    } catch {
+                        print(error)
+                    }
+                }
             }
         }
         .padding()
